2c4ec472a0c8a0a8181e3d4803414782652814a3
guilherme <guiguicaiano10@gmail.com>
2026-09-24 11:21:55 -0300

    feat: codificando e estilizando o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Jogos from './pages/Jogos'
+import Login from './pages/Login'
+import Error from './pages/Error'
 const App = () => {
   return (
-    <>
+    <Router>
+      <div className="min-h-screen flex flex-col justify-between bg-[#141414] pt-4 ">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>   
+          <Route path="/Jogos" element={<Jogos/>}/>
+          <Route path="/Contato" element={<Contato/>}/>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+      </div>
       
-    </>
+    </Router>
   )
 }
 
